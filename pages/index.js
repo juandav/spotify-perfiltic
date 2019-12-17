4017227deb48a2377e2ef10f9696bcef3ef2133a
juandav <e.juandav@gmail.com>
2019-12-16 20:24:35 -0500

    feat(ui): add a login button

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import { HeaderTab } from '../src/components/HeaderTab'
 import Nav from '../src/components/nav'
-import { Hero, Title, Description, Row, Card } from '../src/styles/Home'
+import { Hero, Title, Description, Row, Button } from '../src/styles/Home'
 
 const Home = () => (
   <div>
@@ -11,27 +11,15 @@ const Home = () => (
     </Head>
 
     <Nav />
-    <p>{process.env.SPOTIFY_SCOPES}</p>
 
     <Hero>
-      <Title>Welcome to Next.js!</Title>
+      <Title>Welcome to SPTIC!</Title>
       <Description>
         To get started, edit <code>pages/index.js</code> and save to reload.
       </Description>
 
       <Row>
-        <Card href='https://nextjs.org/docs'>
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </Card>
-        <Card href='https://nextjs.org/learn'>
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </Card>
-        <Card href='https://github.com/zeit/next.js/tree/master/examples'>
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </Card>
+        <Button>Login</Button>
       </Row>
     </Hero>
   </div>
--- src/styles/Home.js
+++ src/styles/Home.js
@@ -48,3 +48,19 @@ export const Card = styled.a`
     color: #333;
   }
 `
+export const Button = styled.button`
+  padding: 15px 50px;
+  text-transform: uppercase;
+  background: #8300ff;
+  border: 2px solid #8300ff;
+  color: #fff;
+  font-weight: 600;
+  font-size: .75rem;
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all .4s;
+  &:hover {
+    opacity: 0.8;
+    box-shadow: 0 14px 26px -12px rgba(131, 0, 255, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(131, 0, 255, 0.2);
+  }
+`
