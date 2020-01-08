e2eb120ec2f97a11a2226a24b9e86f9dd0b258f0
juandav <e.juandav@gmail.com>
2020-01-07 23:16:31 -0500

    feat(store): add redux dev tools

--- src/store/make-store.js
+++ src/store/make-store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { createLogger } from 'redux-logger'
 import { createEpicMiddleware } from 'redux-observable'
 import { rootEpic } from './root-epics'
@@ -7,7 +8,11 @@ import { rootReducer } from './root-reducers'
 export default function initStore (initialState) {
   const epicMiddleware = createEpicMiddleware()
   const logger = createLogger({ collapsed: true })
-  const reduxMiddleware = applyMiddleware(epicMiddleware, logger)
+  const reduxMiddleware = composeWithDevTools(
+    applyMiddleware(
+      epicMiddleware,
+      logger
+    ))
   const store = createStore(rootReducer, initialState, reduxMiddleware)
   epicMiddleware.run(rootEpic)
   return store
